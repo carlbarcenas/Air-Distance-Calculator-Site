4e70e9412a3d33b04cf211321c2f5c9d8925c883
Carl Barcenas <carlanthony.barcenas@marquette.edu>
2023-06-16 00:53:06 -0500

    Implemented Address lookup and map

--- script.js
+++ script.js
@@ -31,22 +31,86 @@ calculate.addEventListener('click', () => {
     outputText.textContent = `Calculated Air Distance: ${result}km`;
 })
 
-// MAP SECTION: --------------------------------------------------------------------------------------
+// MAP AND ADDRESS LOOKUP: --------------------------------------------------------------------------------------
 // Initialize Map
-var myMap = L.map('map', {center:[38, -77], zoom:12, zoomControl:false});
-var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-myMap.addLayer(lyrOSM);
+// New York
+var startlat = 40.75637123;
+var startlon = -73.98545321;
 
-// Initialize Geocoder
-L.Control.geocoder().addTo(myMap);
+var options = {
+    center: [startlat, startlon],
+    zoom: 9
+}
 
-myMap.on('geosearch/showlocation', function(result) {
-    console.log(result.x, result.y);
+document.getElementById('addrLat').value = startlat;
+document.getElementById('addrLon').value = startlon;
+
+var map = L.map('map', options);
+var nzoom = 12;
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: 'OSM' }).addTo(map);
+
+// Place Marker
+var myMarker = L.marker([startlat, startlon], { title: "Coordinates", alt: "Coordinates", draggable: true }).addTo(map).on('dragend', function () {
+    var lat = myMarker.getLatLng().lat.toFixed(8);
+    var lon = myMarker.getLatLng().lng.toFixed(8);
+    var czoom = map.getZoom();
+    if (czoom < 18) { nzoom = czoom + 2; }
+    if (nzoom > 18) { nzoom = 18; }
+    if (czoom != 18) { map.setView([lat, lon], nzoom); } else { map.setView([lat, lon]); }
+    document.getElementById('addrLat').value = lat;
+    document.getElementById('addrLon').value = lon;
+    myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
+});
+
+function chooseAddr(lat1, lng1) {
+    myMarker.closePopup();
+    map.setView([lat1, lng1], 18);
+    myMarker.setLatLng([lat1, lng1]);
+    lat = lat1.toFixed(8);
+    lon = lng1.toFixed(8);
+    document.getElementById('addrLat').value = lat;
+    document.getElementById('addrLon').value = lon;
+    myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
+}
+
+function myFunction(arr) {
+    var out = "<br />";
+    var i;
+
+    if (arr.length > 0) {
+        for (i = 0; i < arr.length; i++) {
+            out += "<div class='address' title='Show Location and Coordinates' onclick='chooseAddr(" + arr[i].lat + ", " + arr[i].lon + ");return false;'>" + arr[i].display_name + "</div>";
+        }
+        document.getElementById('results').innerHTML = out;
+    }
+    else {
+        document.getElementById('results').innerHTML = "Sorry, no results...";
+    }
+
+}
+
+function addr_search() {
+    var inp = document.getElementById("addr");
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + inp.value;
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+// Set Address Coordinates to A or B
+var pointA_btn = document.querySelector('#setA_btn');
+var pointB_btn = document.querySelector('#setB_btn');
+pointA_btn.addEventListener('click', () => {
+    document.querySelector('#lat1').value = document.querySelector('#addrLat').value;
+    document.querySelector('#lon1').value = document.querySelector('#addrLon').value;
 })
-
-// TODO: Get Lat / Long of Address
-/*
-document.addEventListener('click', (e) => {
-    console.log(myMap.getBounds().getCenter()); // This only gets the center of map lat and long
+pointB_btn.addEventListener('click', () => {
+    document.querySelector('#lat2').value = document.querySelector('#addrLat').value;
+    document.querySelector('#lon2').value = document.querySelector('#addrLon').value;
 })
-*/
